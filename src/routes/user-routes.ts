41f6ddc610a2317f1bf726a4c4345b9ca81fc3be
Anderson Leonardo <anderson.scastilho@hotmail.com>
2023-07-30 12:36:00 -0300

    feat: Moved business rule to controller

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,31 +1,10 @@
-import { Request, Response, Router } from "express";
-import { CreateUser } from "use-cases/user/create-user";
-import { PostgresCreateUserRepository } from "../repositories/postgres/postgres-user-repository";
+import { Router } from "express";
+import { CreateUserController } from "use-cases/user/create-user-controller";
 
 const router = Router();
 
-router.post("/users", async (req: Request, res: Response) => {
-  const { email, fullName, password } = req.body;
-  try {
-    const userRepository = new PostgresCreateUserRepository();
-    const createUser = new CreateUser(userRepository);
+const createUserController = new CreateUserController();
 
-    const user = await createUser.execute({
-      userEmail: email,
-      userFullName: fullName,
-      userPassword: password,
-    });
-
-    return res.status(200).json({
-      email: user.userEmail,
-      fullName: user.userFullName,
-      id: user.userId,
-    });
-  } catch (e) {
-    return res.status(400).json({
-      message: "Não foi possivel criar o usuario",
-    });
-  }
-});
+router.post("/users", createUserController.handle);
 
 export { router as userRouter };
