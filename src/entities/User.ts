edabb7a8e47de22a6232f6284756485b10cdf48e
Anderson Leonardo <anderson.scastilho@hotmail.com>
2023-07-30 12:26:00 -0300

    feat: add methoods to encrypted password and compare

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 export interface UserProps {
   userFullName: string;
   userEmail: string;
@@ -7,7 +8,6 @@ export interface UserProps {
 
 export class User {
   private props: UserProps;
-
   get userEmail() {
     return this.props.userEmail;
   }
@@ -19,10 +19,26 @@ export class User {
   get userPassword() {
     return this.props.userPassword;
   }
+
   get userId() {
     return this.props.userId;
   }
+
+  set hashPasswordToUserPassword(hashPassword: string) {
+    this.props.userPassword = hashPassword;
+  }
+
   constructor(props: UserProps) {
     this.props = props;
+  }
+
+  async encryptedPassword(password: string) {
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    return hashPassword;
+  }
+
+  async comparePasswords(password: string) {
+    return bcrypt.compare(password, this.userPassword);
   }
 }
---
+++ src/use-cases/user/create-user-controller.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+
+import { CreateUser } from "./create-user";
+import { PostgresCreateUserRepository } from "repositories/postgres/postgres-user-repository";
+
+export class CreateUserController {
+  async handle(req: Request, res: Response) {
+    const { email, fullName, password } = req.body;
+
+    try {
+      const userRepository = new PostgresCreateUserRepository();
+      const createUser = new CreateUser(userRepository);
+
+      const user = await createUser.execute({
+        userEmail: email,
+        userFullName: fullName,
+        userPassword: password,
+      });
+
+      return res.status(200).json({
+        email: user.userEmail,
+        fullName: user.userFullName,
+        id: user.userId,
+      });
+    } catch (e) {
+      return res.status(400).json({
+        message: "Não foi possivel criar o usuario",
+      });
+    }
+  }
+}
