3ba2168a0473cac697ff9c945d8d7cff6b6022bd
Anderson Leonardo <anderson.scastilho@hotmail.com>
2023-09-06 12:51:38 -0300

    feat: return page of sucess or failure

---
+++ src/html/validemail.ts
@@ -0,0 +1,63 @@
+export const validEmail = `<!DOCTYPE html>
+<html>
+<head>
+    <title>Verificação de E-mail Concluída</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+        }
+
+        h1 {
+            background-color: #007BFF;
+            color: #fff;
+            padding: 20px;
+            text-align: center;
+        }
+
+        p {
+            text-align: center;
+            font-size: 18px;
+            margin-top: 20px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Verificação de E-mail Concluída com Sucesso</h1>
+    <p>O seu endereço de e-mail foi validado com sucesso. Agora você pode acessar nossos serviços.</p>
+</body>
+</html>`;
+
+export const invalidEmail = `<!DOCTYPE html>
+<html>
+<head>
+    <title>Validação de E-mail</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+        }
+
+        h1 {
+            background-color: #FF4500;
+            color: #fff;
+            padding: 20px;
+            text-align: center;
+        }
+
+        p {
+            text-align: center;
+            font-size: 18px;
+            margin-top: 20px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Validação de E-mail Falhou</h1>
+    <p>Não foi possível validar o endereço de e-mail. Por favor, verifique o endereço e tente novamente.</p>
+</body>
+</html>`;
--- src/use-cases/email/verify/verify-email-controller.ts
+++ src/use-cases/email/verify/verify-email-controller.ts
@@ -2,18 +2,15 @@ import { NextFunction, Request, Response } from "express";
 import { z } from "zod";
 import { VerifyEmail } from "./verify-email";
 import { PostgresShowUserPerUserIdRepository } from "repositories/postgres/user/postgres-show-user-userId-repository";
-
+import { validEmail, invalidEmail } from "../../../html/validemail";
 const paramRequest = z.string();
 
 export default class VerifyEmailController {
   async handle(req: Request, res: Response, next: NextFunction) {
     try {
       const token = paramRequest.parse(req.query.token);
-
       if (!token) {
-        return res
-          .status(401)
-          .send("<h1>Não foi possivel validar seu e-mail</h1>");
+        return res.status(401).send(`${invalidEmail}`);
       }
 
       const showUserPerIdRepository = new PostgresShowUserPerUserIdRepository();
@@ -23,12 +20,10 @@ export default class VerifyEmailController {
       const isValidToken = await verifyEmail.execute(fullFieldToken);
 
       if (isValidToken === false) {
-        return res
-          .status(400)
-          .send("<h1>Não foi possivel validar seu e-mail</h1>");
+        return res.status(400).send(`${invalidEmail}`);
       }
 
-      return res.status(200).send(`<h1>Email validado com sucesso</h1>`);
+      return res.status(200).send(`${validEmail}`);
     } catch (e) {
       return res
         .status(400)
