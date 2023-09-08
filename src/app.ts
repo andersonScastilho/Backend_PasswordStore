a25e0cd760ed9b4f020da29ba9731d5d64aa5266
Anderson Leonardo <anderson.scastilho@hotmail.com>
2023-09-08 16:47:30 -0300

    feat: configurando cors

--- src/app.ts
+++ src/app.ts
@@ -27,20 +27,20 @@ class App {
   private _middlewares() {
     this.app.use(express.json());
 
-    // const allowedOrigin = "https://passtorage.vercel.app";
+    const allowedOrigin = "https://passtorage.vercel.app";
 
-    // const corsOptions = {
-    //   origin: (
-    //     origin: string | undefined,
-    //     callback: (err: Error | null, allow?: boolean) => void
-    //   ) => {
-    //     if (origin === allowedOrigin) {
-    //       callback(null, true);
-    //     } else {
-    //       callback(new Error("Acesso não permitido por CORS"));
-    //     }
-    //   },
-    // };
+    const corsOptions = {
+      origin: (
+        origin: string | undefined,
+        callback: (err: Error | null, allow?: boolean) => void
+      ) => {
+        if (origin === allowedOrigin) {
+          callback(null, true);
+        } else {
+          callback(new Error("Acesso não permitido por CORS"));
+        }
+      },
+    };
 
     this.app.use(cors());
     this.app.use(helmet());
