fd07c992a240d2bcfa1d73f23aa7c7f5f0f2873e
Anderson Leonardo <anderson.scastilho@hotmail.com>
2023-09-13 12:02:08 -0300

    corrigindo updated password

--- src/use-cases/storage/update/update-storage.ts
+++ src/use-cases/storage/update/update-storage.ts
@@ -4,6 +4,7 @@ import {
   UpdateStorageParams,
   UpdateStorageRepository,
 } from "repositories/storage/update-storage-repository";
+import { encrypt } from "utils/crypt";
 
 export class UpdateStorage {
   constructor(
@@ -20,10 +21,21 @@ export class UpdateStorage {
     userId,
     password,
   }: UpdateStorageParams) {
-    const storage = await this.showStorageRepository.show(storageId, userId);
+    const storageSchema = await this.showStorageRepository.show(
+      storageId,
+      userId
+    );
 
-    if (!storage) {
+    if (!storageSchema) {
       throw Error("Storage not found");
+    }
+    let passwordEncrypted;
+
+    if (password) {
+      const { iv, content, tag } = encrypt(password);
+      const encryptedPassword = `${iv}:${content}:${tag}`;
+
+      passwordEncrypted = encryptedPassword;
     }
 
     const updatedStorage = await this.updateStorageReposirory.update({
@@ -33,19 +45,19 @@ export class UpdateStorage {
       description: description,
       link: link,
       usageLocation: usageLocation,
-      password: password,
+      password: password ? passwordEncrypted : undefined,
     });
 
-    const instanceStorage = new Storage({
+    const storage = new Storage({
       account: updatedStorage.account,
       password: "",
       storageId: updatedStorage.id,
       usageLocation: updatedStorage.usageLocation,
       userId: updatedStorage.userId,
-      description: updatedStorage.description || "",
-      link: updatedStorage.link || "",
+      description: updatedStorage.description,
+      link: updatedStorage.link,
     });
 
-    return instanceStorage;
+    return storage;
   }
 }
