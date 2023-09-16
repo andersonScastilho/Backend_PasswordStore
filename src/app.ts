520ddd5c02ac20f06e8b1d31b3bb6f9472f0a0e6
Anderson Leonardo <anderson.scastilho@hotmail.com>
2023-09-16 18:30:09 -0300

    use cors

--- src/app.ts
+++ src/app.ts
@@ -27,8 +27,22 @@ class App {
 
   private _middlewares() {
     this.app.use(express.json());
+    const allowedOrigin = "https://passtorage.vercel.app";
 
-    this.app.use(cors());
+    const corsOptions = {
+      origin: (
+        origin: string | undefined,
+        callback: (err: Error | null, allow?: boolean) => void
+      ) => {
+        if (origin === allowedOrigin) {
+          callback(null, true);
+        } else {
+          callback(new Error("Acesso não permitido por CORS"));
+        }
+      },
+    };
+
+    this.app.use(cors(corsOptions));
     this.app.use(helmet());
   }
 
