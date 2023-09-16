9c386205c4914d547ac724f2beabcbf928c3cdff
Anderson Leonardo <anderson.scastilho@hotmail.com>
2023-09-16 18:27:46 -0300

    Feat: use password to delete storage

--- src/use-cases/storage/delete/delete-storage-controller.ts
+++ src/use-cases/storage/delete/delete-storage-controller.ts
@@ -3,25 +3,34 @@ import { PostgresDeleteStorageRepository } from "repositories/postgres/storage/p
 import { PostgresShowStorageRepository } from "repositories/postgres/storage/postgres-show-storage-repository";
 import { DeleteStorage } from "./delete-storage";
 import { z } from "zod";
+import { PostgresShowUserPerUserIdRepository } from "repositories/postgres/user/postgres-show-user-userId-repository";
 
 const ParamsSchema = z.object({
   userId: z.string(),
   storageId: z.string(),
 });
+
+const BodySchema = z.object({
+  password: z.string(),
+});
 export class DeleteStorageController {
   async handle(req: Request, res: Response, next: NextFunction) {
     try {
       const { userId, storageId } = ParamsSchema.parse(req.params);
+      const { password } = BodySchema.parse(req.body);
 
       const deleteStorageRepository = new PostgresDeleteStorageRepository();
       const showStorageRepository = new PostgresShowStorageRepository();
+      const showUserPerUserIdRepository =
+        new PostgresShowUserPerUserIdRepository();
 
       const deleteStorage = new DeleteStorage(
         deleteStorageRepository,
-        showStorageRepository
+        showStorageRepository,
+        showUserPerUserIdRepository
       );
 
-      await deleteStorage.execute(storageId, userId);
+      await deleteStorage.execute(storageId, userId, password);
 
       return res.status(200).json({});
     } catch (e) {
--- src/use-cases/storage/delete/delete-storage.ts
+++ src/use-cases/storage/delete/delete-storage.ts
@@ -1,16 +1,42 @@
+import { User } from "entities/User";
+import { UserSchema } from "models/user-schema";
 import { DeleteStorageRepository } from "repositories/storage/delete-storage-repository";
 import { ShowStorageRepository } from "repositories/storage/show-storage-repository";
+import { ShowUserPerUserIdRepository } from "repositories/user/show-user-userId-repository";
 
 export class DeleteStorage {
   constructor(
     private deleteStorageRepository: DeleteStorageRepository,
-    private showStorageRepository: ShowStorageRepository
+    private showStorageRepository: ShowStorageRepository,
+    private showUserPerUserIdRepository: ShowUserPerUserIdRepository
   ) {}
-  async execute(storageId: string, userId: string) {
-    const storage = await this.showStorageRepository.show(storageId, userId);
+  async execute(storageId: string, userId: string, password: string) {
+    const storageSchema = await this.showStorageRepository.show(
+      storageId,
+      userId
+    );
 
-    if (!storage) {
+    if (!storageSchema) {
       throw Error("Storage not found");
+    }
+
+    const userSchema = await this.showUserPerUserIdRepository.show(userId);
+
+    if (!userSchema) {
+      throw Error("User not found");
+    }
+
+    const user = new User({
+      userEmail: userSchema.email,
+      userFullName: userSchema.fullName,
+      userId: userSchema.id,
+      userPassword: userSchema.password_hash,
+    });
+
+    const isMatchPassword = user.comparePasswords(password);
+
+    if (!isMatchPassword) {
+      throw Error("Invalid password");
     }
 
     await this.deleteStorageRepository.delete(storageId);
